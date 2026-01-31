910924c64af275d5c614a96bb5ef56a9b7c169d3
Jyoti-24-05 <jyotika918273@gmail.com>
2026-01-31 20:59:50 +0530

    update

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,21 +1,22 @@
 import mongoose from "mongoose";
 
-const userSchema= new mongoose.Schema({
-  fullName:{
-    type:String,
-    required:true,
-  },
-  imageUrl:{
-    type:String,
-    required:true,
-  },
-  clerkId:{
-    type:String,
-    required:true,
-    unique:true,
-  },
-},
-  { timestamp: true}//created at updated at
+const userSchema = new mongoose.Schema(
+	{
+		fullName: {
+			type: String,
+			required: true,
+		},
+		imageUrl: {
+			type: String,
+			required: true,
+		},
+		clerkId: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+	},
+	{ timestamps: true } //  createdAt, updatedAt
 );
 
-export const User= mongoose.model("User", userSchema);
+export const User = mongoose.model("User", userSchema);
